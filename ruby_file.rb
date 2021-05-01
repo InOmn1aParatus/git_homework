b5b269fabadb5dadde5b695627b3ecc13487c955
Matt Kragen <mattdkragen@gmail.com>
2021-04-30 23:39:10 -0700

    Create Pet class

--- ruby_file.rb
+++ ruby_file.rb
@@ -1,1 +1,22 @@
-puts "Looks like there's a first line now!"
+class Pet
+
+  def initialize(name, age, type, sound)
+    @name = name
+    @age = age
+    @type = type
+    @sound = sound
+  end
+
+  def make_sound
+    puts "#{@sound}!"
+  end
+
+  def who_are_you
+    puts "Hi there! My name is #{@name}. I'm a #{@age}-year-old #{@type}. I like to go '#{@sound}'!"
+  end
+
+end
+
+jeffrey = Pet.new("Jeffrey", 4, "dog", "Woof")
+
+jeffrey.who_are_you
